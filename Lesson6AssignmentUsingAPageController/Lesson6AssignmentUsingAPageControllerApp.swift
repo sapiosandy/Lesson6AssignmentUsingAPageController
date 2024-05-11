a6fb044ff77bf6948517556d96fbf391959ede18
Sandy Gomez <sgomez1029@gmail.com>
2024-05-10 18:15:51 -0700

    Initial Commit

---
+++ Lesson6AssignmentUsingAPageController/Lesson6AssignmentUsingAPageControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson6AssignmentUsingAPageControllerApp.swift
+//  Lesson6AssignmentUsingAPageController
+//
+//  Created by Sandra Gomez on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Lesson6AssignmentUsingAPageControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
